1ee1352c58f704fa66f127cd5f6af00cdbf89c8c
OleksandrChaika13 <axelrod1347@gmail.com>
2023-08-11 13:44:32 +0300

    Adding "Order" class

--- src/app/orders/Order.java
+++ src/app/orders/Order.java
@@ -1,4 +1,32 @@
 package app.orders;
 
+import app.items.Item;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
+
+    List<Item> items = new ArrayList<>();
+
+    public void addItem(Item item) {
+        items.add(item);
+    }
+
+    public float getCost() {
+        float cost = 0.0f;
+
+        for (Item item : items) {
+            cost += item.price();
+        }
+        return cost;
+    }
+
+    public void showItems() {
+        for (Item item : items) {
+            System.out.print("Item: " + item.name());
+            System.out.print(", Producer: " + item.producer().pack());
+            System.out.println(", Price: USD " + item.price());
+        }
+    }
 }
