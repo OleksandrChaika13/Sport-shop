783459b5316b9a89df9175196ccd5c5967c22dc8
OleksandrChaika13 <axelrod1347@gmail.com>
2023-08-11 13:45:42 +0300

    Adding "BuilderPattern" class

--- src/app/orders/BuilderPattern.java
+++ src/app/orders/BuilderPattern.java
@@ -1,4 +1,19 @@
 package app.orders;
 
 public class BuilderPattern {
+
+    public static void bPattern() {
+
+        OrderBuilder orderBuilder = new OrderBuilder();
+
+        Order orderA = orderBuilder.prepareOrderA();
+        System.out.println("Order A");
+        orderA.showItems();
+        System.out.println("Total Cost: USD " + orderA.getCost());
+
+        Order orderB = orderBuilder.prepareOrderB();
+        System.out.println("\nOrder B");
+        orderB.showItems();
+        System.out.println("Total Cost: USD " + orderB.getCost());
+    }
 }
