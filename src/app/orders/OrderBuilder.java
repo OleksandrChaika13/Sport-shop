c4b14d0d46ec6195e271b03576685dbe825efb05
OleksandrChaika13 <axelrod1347@gmail.com>
2023-08-11 13:45:06 +0300

    Adding "OrderBuilder" class

--- src/app/orders/OrderBuilder.java
+++ src/app/orders/OrderBuilder.java
@@ -1,4 +1,23 @@
 package app.orders;
 
+import app.goods.BlackCap;
+import app.goods.BlueShort;
+import app.goods.RedShort;
+import app.goods.WhiteCap;
+
 public class OrderBuilder {
+
+    public Order prepareOrderA() {
+        Order order = new Order();
+        order.addItem(new RedShort());
+        order.addItem(new WhiteCap());
+        return order;
+    }
+
+    public Order prepareOrderB() {
+        Order order = new Order();
+        order.addItem(new BlueShort());
+        order.addItem(new BlackCap());
+        return order;
+    }
 }
